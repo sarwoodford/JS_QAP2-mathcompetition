94f521c0dae1169dea45b3ccf1651adaf8b8b24e
sara woodford <sarawoodford6@gmail.com>
2024-10-13 22:16:02 -0230

    streaks tracked, leaderboard created and new routes
    streak is now stored in memory and displays 0 or a
    message indicating there is no streak, and ends if
    and answer is incorrect, leaderboard tracks top 10
    streaks and new routes for completion and leaderboard
    pages created

--- index.js
+++ index.js
@@ -7,27 +7,55 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true })); // For parsing form data
 app.use(express.static('public')); // To serve static files (e.g., CSS)
 
+// initialize leaderboard to track top 10 streaks
+let leaderboard = [];
+
 
 //Some routes required for full functionality are missing here. Only get routes should be required
 app.get('/', (req, res) => {
-    res.render('index');
+    // display recent streak or a message indicating that there aren't any
+    const recentStreak = req.query.streak || 0; // streak default 0 if none recorded
+    const message = recentStreak > 0 ? `Latest Streak: ${recentStreak}` : `No Recent Streaks`;
+    res.render('index', {message, leaderboard});
 });
 
 app.get('/quiz', (req, res) => {
     const question = mathUtilities.getQuestion();
+    const streak = req.query.streak || 0; // streak default is 0 if none recorded
 
-    res.render('quiz', { question });
+    res.render('quiz', { question, streak });
 });
 
 //Handles quiz submissions.
 app.post('/quiz', (req, res) => {
-    const { answer } = req.body;
+    const { answer, streak } = req.body;
+    const question = mathUtilities.getQuestion();
+    const isCorrect = mathUtilities.isCorrectAnswer( question, answer );
+
+    let newStreak = isCorrect ? parseInt(streak) + 1 : 0;
+
+    // make sure incorrect answers reset streak 
+    if (!isCorrect){
+        leaderboard.push({ streak: parseInt( streak ), date: new Date().toLocaleString(), correctAnswers: parseInt(streak) });
+        leaderboard = leaderboard.sort(( a, b ) => b.streak - a.streak).slice(0, 10); // keep top 10 streaks
+    }
+
+    redirect(`/quiz?streak=${newStreak}`);
     console.log(`Answer: ${answer}`);
 
     //answer will contain the value the user entered on the quiz page
     //Logic must be added here to check if the answer is correct, then track the streak and redirect properly
-    //By default we'll just redirect to the homepage again.
-    res.redirect('/');
+    
+});
+
+app.get('/completion', ( req, res ) => {
+    const currentStreak = req.query.streak || 0; // streak default 0 
+
+    res.render('completion', { currentStreak });
+});
+
+app.get('/leaderboard', ( req, res ) => {
+    res.render('leaderboard', { leaderboard });
 });
 
 // Start the server
